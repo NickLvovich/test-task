182596d30abb6c72122ebc312cedc7db0025437e
nik <kol-3cl@outlook.com>
2020-02-20 15:42:19 +0200

    add nodemon, config files and private information into dev.js

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-mongoose.connect('mongodb+srv://nick:alfred278@cluster0-2sfr3.gcp.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true
-}).then(() => console.log('pajilaya baza zdes')).catch(err => console.log(err))
+const config = require('./config/key')
 
-app.get('/', (req,res) => {
-    res.send('hello ')
+const { User } = require("./models/user");
+
+mongoose
+  .connect(config.mongoURI,
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("pajilaya baza zdes"))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.json("kavosho")
 })
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+        success:true
+    });
+  });
+
+});
 
 app.listen(5000);
