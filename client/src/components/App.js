d5e0d0ee846023aedfeaea23d40aa5141f270628
nik <kol-3cl@outlook.com>
2020-02-20 23:40:18 +0200

    react router and connection to components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './home'
+import Users from './users'
+import Friends from './friends'
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div>
+      <Switch>
+        <Route path="/" component={Home}/>
+        <Route path="/users" component={Users}/>
+        <Route path="/friends"component={Friends}/>
+      </Switch>
     </div>
   );
 }
