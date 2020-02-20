f4b81f515afe2cb26fa032b1b7307cfb01affe0e
nik <kol-3cl@outlook.com>
2020-02-20 19:05:40 +0200

    tocken for user

--- index.js
+++ index.js
@@ -4,16 +4,14 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
-const config = require('./config/key')
+const config = require("./config/key");
 
 const { User } = require("./models/user");
 
 mongoose
-  .connect(config.mongoURI,
-    {
-      useNewUrlParser: true
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true
+  })
   .then(() => console.log("pajilaya baza zdes"))
   .catch(err => console.log(err));
 
@@ -21,19 +19,43 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.json("kavosho")
-})
+app.get("/", (req, res) => {
+  res.json("kavosho");
+});
 
 app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
   user.save((err, userData) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
-        success:true
+      success: true
     });
   });
+});
 
+app.post("/api/user/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        messsage: "Auth failed, email not found"
+      });
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({ loginSuccess: false, messsage: "wrong password" });
+      }
+    });
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res
+        .cookie("x_auth", user.token)
+        .status(200)
+        .json({
+          loginSuccess: true
+        });
+    });
+  });
 });
 
 app.listen(5000);
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const saltRounds = 10;
 
@@ -38,13 +39,32 @@ userSchema.pre("save", function(next) {
       bcrypt.hash(user.password, salt, function(err, hash) {
         if (err) return next(err);
         user.password = hash;
+        next()
         // Store hash in your password DB.
       });
     });
   } else {
-      next ()
+    next();
   }
 });
+
+userSchema.methods.comparePassword = function(plainPassword, callback) {
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function(callback) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), "secret");
+
+  user.token = token;
+  user.save(function(err, user){
+      if(err) return callback(err)
+      callback(null, user)
+  })
+};
 
 const User = mongoose.model("User", userSchema);
 
