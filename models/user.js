a348f24245b58f0f1a03c7904843718984fc199f
nik <kol-3cl@outlook.com>
2020-02-20 16:18:43 +0200

    bcrypt and hash

--- models/user.js
+++ models/user.js
@@ -1,31 +1,51 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
-    name: {
-        type:String,
-        maxlength:50
-    },
-    email: {
-        type:String,
-        trim:true,
-        unique: true
-    },
-    password: {
-        type: String,
-        minlength: 5
-    },
-    lastname: {
-        type: String,
-        maxlength: 50
-    },
-    token: {
-        type: String
-    },
-    tokenExp: {
-        type: Number
-    }
-})
+  name: {
+    type: String,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    minlength: 5
+  },
+  lastname: {
+    type: String,
+    maxlength: 50
+  },
+  token: {
+    type: String
+  },
+  tokenExp: {
+    type: Number
+  }
+});
 
-const User = mongoose.model('User', userSchema)
+userSchema.pre("save", function(next) {
+  var user = this;
 
-module.exports = { User }
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        // Store hash in your password DB.
+      });
+    });
+  } else {
+      next ()
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
