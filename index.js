795e35c8af4888f91684572e381a5940db108e8d
nik <kol-3cl@outlook.com>
2020-02-20 20:55:11 +0200

    add auth and login

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
+
 const config = require("./config/key");
 
 const { User } = require("./models/user");
+const { auth } = require("./middleware/auth")
 
 mongoose
   .connect(config.mongoURI, {
@@ -19,21 +21,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.json("kavosho");
+app.get("/api/users/auth", auth, (req, res) => {
+    res.status(200).json({
+        _id:req._id,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname:req.user.lastname
+    })
 });
 
 app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
-  user.save((err, userData) => {
+  user.save((err, doc) => {
     if (err) return res.json({ success: false, err });
-    return res.status(200).json({
-      success: true
+    res.status(200).json({
+      success: true,
+      userData: doc
     });
   });
 });
 
-app.post("/api/user/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user)
       return res.json({
@@ -57,5 +66,14 @@ app.post("/api/user/login", (req, res) => {
     });
   });
 });
+
+app.get("/api/users/logout", (req, res) =>{
+    User.findByIdAndUpdate({_id: req.user._id}, { token: "" }, (err, doc) => {
+        if(err) return res.json({ success:false, err })
+        return res.status(200).send({
+            success: true
+        })
+    })
+})
 
 app.listen(5000);
